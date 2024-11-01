555d8f0f8c796701b8f2ffa766761dc6b7a028ff
ThiagoVilani <thiagovilani19@gmail.com>
2024-11-01 09:58:52 -0300

    Solucion de pequeños errores

--- loginAdm.js
+++ loginAdm.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("Ingresar").addEventListener("click",()=>{
+        //Validar credenciales
         console.log("apreto el boton");
         window.location.href = "./abm-admin.html";
     });
--- productos-listados.js
+++ productos-listados.js
@@ -4,23 +4,28 @@ let indicePrimerProducto = 0;
 
 obtenerProductos();
 
-
-document.getElementsByClassName("btn-contacto")[0].addEventListener("click",()=>{
-    window.location.href = "./contacto.html";
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementsByClassName("btn-opcion-nav")[0].addEventListener("click",function(){
+        obtenerProductos("casco");
+    });
+    document.getElementsByClassName("btn-opcion-nav")[1].addEventListener("click",function(){
+        obtenerProductos("campera");
+    });
+    
+    document.getElementById("pagina-anterior").addEventListener("click",function(){
+        obtenerProductos("anterior");
+    });
+    document.getElementById("pagina-siguiente").addEventListener("click",function(){
+        obtenerProductos("siguiente");
+    });
 });
-document.getElementById("pagina-anterior").addEventListener("click",function(){
-    obtenerProductos("anterior");
-});
-document.getElementById("pagina-siguiente").addEventListener("click",function(){
-    obtenerProductos("siguiente");
-});
 
 
 
-async function obtenerProductos(direccion=null){
+async function obtenerProductos(tipo=null,direccion=null){
     try{
         const data = await Producto.TraerProductos();
-        CargarProductos(indicePrimerProducto,data,direccion);
+        CargarProductos(indicePrimerProducto,data,tipo,direccion);
     }catch(error){
         console.error("Error:", error);
     }
@@ -39,19 +44,22 @@ function EstablecerIndiceInicial(indiceInicial,direccion){
     return indiceInicial;
 }
 
-
+//Poner filtro de tipo
 function CargarProductos(indiceInicial,data,direccion=null){
+    document.getElementsByClassName("grid-productos")[0].innerHTML = "";
     if(direccion){
-        document.getElementsByClassName("grid-productos")[0].innerHTML = "";
         indiceInicial = EstablecerIndiceInicial(indiceInicial,direccion);
     }
-    console.log(indiceInicial);
     for(let i=indiceInicial;i<indiceInicial+12;i++){
         if(i<data.length){
-            let producto = new Producto(data[i]["img"],data[i]["marca"],data[i]["modelo"],data[i]["precio"],data[i]["tipo"],data[i]["estado"]);
-            let elementoHTML = Producto.CrearElementoProductoGrilla(producto);
-            document.getElementsByClassName("grid-productos")[0].appendChild(elementoHTML)
+            CrearYUbicar(data,i);
         }
     }
     return indiceInicial;
+}
+
+function CrearYUbicar(data,i){
+    let producto = new Producto(data[i]["img"],data[i]["marca"],data[i]["modelo"],data[i]["precio"],data[i]["tipo"],data[i]["estado"]);
+    let elementoHTML = Producto.CrearElementoProductoGrilla(producto);
+    document.getElementsByClassName("grid-productos")[0].appendChild(elementoHTML);
 }
