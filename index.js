0c8cd0c110689eeacd1a1c20e27b94563bf793de
VGdC15 <LIbertad2801>
2024-11-08 16:12:57 -0300

    Guardar nombre de input en local storage

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-
+/* 
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
     if (this.checkValidity()) {
         window.location.href = "productos-listados.html";
     } else {
-        this.reportValidity();
+        this.reportValidity(); 
     }
 });
+*/
 
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    if (this.checkValidity()) {
+        // Obtener el valor ingresado en el input
+        const username = document.getElementById('input').value;
+
+        // Guardar el nombre de usuario en local storage
+        localStorage.setItem('username', username);
+
+        // Redirigir a la página de productos listados
+        window.location.href = "productos-listados.html";
+    } else {
+        this.reportValidity(); 
+    }
+});
