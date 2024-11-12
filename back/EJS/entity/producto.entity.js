af7a89f8013febe4ff3ea31419f38613165bce86
ThiagoVilani <thiagovilani19@gmail.com>
2024-11-12 13:15:23 -0300

    Pagina de productos-listados desde ejs (no terminada)

---
+++ back/EJS/entity/producto.entity.js
@@ -0,0 +1,47 @@
+const sequelize = require("../db/sequelize");
+const { DataTypes } = require("sequelize");
+
+const ProductoSequelize = sequelize.define(
+  "Producto",
+  {
+    idproductos: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    marca: {
+      type: DataTypes.STRING,
+      allowNull: false, // NOT NULL
+    },
+    modelo: {
+      type: DataTypes.STRING,
+      allowNull: false, // NOT NULL
+    },
+    imagen:{
+        type: DataTypes.STRING,
+        allowNull:false,
+    },
+    precio: {
+      type: DataTypes.INTEGER,
+      allowNull: false, // NOT NULL
+    },
+    tipo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    estado: {
+      defaultValue: true,
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = ProductoSequelize;
---
+++ back/EJS/public/javascripts/nav.js
@@ -0,0 +1,5 @@
+
+document.getElementsByClassName("logo-nav")[0].addEventListener("click",()=>{
+    //Si esta en "index.html" no deberia funcionar porque no puso su nombre todavia
+    window.location.href = "./productos-listados.html";
+});
