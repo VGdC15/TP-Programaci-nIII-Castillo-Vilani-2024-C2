9ba26025f26e76e5453c0b426fa7615c56ad31a8
VGdC15 <LIbertad2801>
2024-11-23 19:35:07 -0300

    guardado de ticket en pdf terminado

--- front/javascripts/mainPdf.js
+++ front/javascripts/mainPdf.js
@@ -1,12 +1,24 @@
  const { jsPDF } = window.jspdf;
 
  const doc = new jsPDF();
+
  function guardar() {
-   doc.html(document.getElementById("ticket"), {
-     callback: function (doc) {
-       doc.save("Ticket Rider's Edge.pdf");
-     },
-     x: 10,
-     y: 10,
-   });
- }
+  const ticketElement = document.getElementById("ticket");
+
+  html2canvas(ticketElement).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const doc = new jsPDF(); 
+
+    const imgWidth = 400; 
+    const imgHeight = (canvas.height * imgWidth) / canvas.width; 
+
+    // posición centrada
+    const x = (210 - imgWidth) / 2; // horizontal
+    const y = (297 - imgHeight) / 7; // vertical
+
+    doc.addImage(imgData, "PNG", x, y, imgWidth, imgHeight);
+    doc.save("Ticket Rider's Edge.pdf");
+  });
+}
+
+
